5e2e3be7fe9284283b76fae8573c4054f971fcbb
pinakin kansara <pinakin.kansara@mastercard.com>
2017-10-13 11:34:42 +0530

    - Annotated class with @DataMine

--- app/src/main/java/com/mastercard/www/aprocessor/PreferenceData.java
+++ app/src/main/java/com/mastercard/www/aprocessor/PreferenceData.java
@@ -1,4 +1,9 @@
 package com.mastercard.www.aprocessor;
 
+import android.content.Context;
+
+import com.mastercard.www.library.DataMine;
+
+@DataMine(fileName = "preference_test",mode = Context.MODE_PRIVATE)
 public class PreferenceData {
 }
