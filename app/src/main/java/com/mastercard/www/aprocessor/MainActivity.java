20e961c786c3f728497960fb115ff70099cf326c
pinakin kansara <pinakin.kansara@mastercard.com>
2017-10-13 11:26:58 +0530

    - removed annotation

--- app/src/main/java/com/mastercard/www/aprocessor/MainActivity.java
+++ app/src/main/java/com/mastercard/www/aprocessor/MainActivity.java
@@ -3,9 +3,6 @@ package com.mastercard.www.aprocessor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.mastercard.www.library.DataMine;
-
-@DataMine
 public class MainActivity extends AppCompatActivity {
 
     @Override
