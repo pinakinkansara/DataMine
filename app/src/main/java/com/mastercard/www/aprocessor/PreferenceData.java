a9baba0f9cb2f3089452105504f41036b9e13296
Kansara, Pinakin <Pinakin.Kansara@mastercard.com>
2017-11-09 15:24:22 +0530

    - sample Preference generator class

--- app/src/main/java/com/mastercard/www/aprocessor/PreferenceData.java
+++ app/src/main/java/com/mastercard/www/aprocessor/PreferenceData.java
@@ -2,8 +2,12 @@ package com.mastercard.www.aprocessor;
 
 import android.content.Context;
 
+import com.mastercard.www.library.DataKey;
 import com.mastercard.www.library.DataMine;
 
 @DataMine(fileName = "preference_test",mode = Context.MODE_PRIVATE)
 public class PreferenceData {
+
+    @DataKey(key = "key_for_test")
+    private int test;
 }
