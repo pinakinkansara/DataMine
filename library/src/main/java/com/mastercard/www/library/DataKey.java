9224db28d5a479a8937573d9d2058d9b201bbcf6
Kansara, Pinakin <Pinakin.Kansara@mastercard.com>
2017-11-09 15:21:48 +0530

    - DataKey annotation declared

---
+++ library/src/main/java/com/mastercard/www/library/DataKey.java
@@ -0,0 +1,22 @@
+package com.mastercard.www.library;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.lang.model.element.Element;
+
+/**
+ * Created by e064173 on 09/11/17.
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface DataKey {
+
+    /**
+     * Preference key for the data to be stored & retrieved.
+     * @return key
+     */
+    String key();
+}
