f766e7fae3ca3678d617face445e5ebf1b3eb3e1
Kansara, Pinakin <Pinakin.Kansara@mastercard.com>
2017-11-10 13:12:04 +0530

    - Sample usage for generated code

--- app/src/main/java/com/datamine/www/aprocessor/MainActivity.java
+++ app/src/main/java/com/datamine/www/aprocessor/MainActivity.java
@@ -2,12 +2,23 @@ package com.datamine.www.aprocessor;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PreferenceRepository repository = new PreferenceRepository(this);
+        repository.puttest(10);
+
+        int val = repository.gettest(-1);
+        Log.d(TAG, "Value obtained from preference : "+val);
     }
 }
